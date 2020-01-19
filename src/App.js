72a47264d0cdfae589610a3581e9a842e30c1d60
ajLapid718 <ajLapid718@gmail.com>
2020-01-18 22:58:50 -0500

    set up form for adding an item

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      price: ""
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <form>
+            <label>Name:</label>
+            <input name="name" type="text" onChange={this.handleChange} value={this.state.name}></input>
+            <br />
+            <br />
+            <label>Price:</label>
+            <input name="price" type="text" onChange={this.handleChange} value={this.state.price}></input>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
